a1a3dbd86328360fb970d14a47c5df83f5274f47
Caio Eduardo <caio-boos>
2023-07-12 22:00:08 -0300

    a

---
+++ src/components/ListPasswords/ListPasswords.tsx
@@ -0,0 +1,12 @@
+export default function ListPasswords(){
+    return (
+        <div className="w-full block md:flex bg-primary ">
+            <div className="w-full md:w-[50%] bg-secondary">
+                div1
+            </div>
+            <div className="w-full md:w-[50%] bg-secondary">
+              div2  
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/places/[slug].tsx
@@ -0,0 +1,10 @@
+import LayoutHome from '@/components/Home/Layout'
+import Head from 'next/head'
+import Image from 'next/image'
+import ListPasswords from '@/components/ListPasswords/ListPasswords'
+
+export default function Places() {
+  return (
+    <ListPasswords />
+  )
+}
